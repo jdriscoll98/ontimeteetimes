be388204d3c9965ab49e2c424d8523eaabf52bb9
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2024-01-20 17:42:42 -0500

    Update route.ts

--- src/app/api/times/route.ts
+++ src/app/api/times/route.ts
@@ -4,7 +4,7 @@ import { getAllTeeTimes, getAuthToken } from "@/lib/golf";
 export async function POST(req: Request) {
     const { email, date } = await req.json();
     const auth = await getAuth({ email })
-    if (!auth) return null;
+    if (!auth) return Response.json({});
     const {data: { "booking_class_ids": booking_class_ids }}= await getAuthToken(auth);
     const times = await getAllTeeTimes({
         date,
